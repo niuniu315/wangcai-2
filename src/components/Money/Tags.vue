4a270d76639c20f884af952a2fa78c932b440fcb
niuniu315 <454378439@qq.com>
2022-05-30 21:30:54 +0800

    可以选中和取消tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -4,7 +4,11 @@
       <button>新增标签</button>
     </div>
     <ul class="tagsul">
-      <li v-for="tag in dataSource" :key="tag">{{tag}}</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected: selectedTags.indexOf(tag) >= 0}"
+          @click="button(tag)"
+      >{{ tag }}
+      </li>
     </ul>
   </div>
 </template>
@@ -14,8 +18,19 @@ import {Component, Prop} from 'vue-property-decorator';
 import Vue from 'vue';
 
 @Component
-export default class Tags extends Vue{
+export default class Tags extends Vue {
   @Prop() dataSource: string[] | undefined;
+  selectedTags: string[] = [];
+
+  button(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    } else {
+      this.selectedTags.push(tag);
+    }
+  }
+
 };
 </script>
 
@@ -32,7 +47,8 @@ export default class Tags extends Vue{
     flex-wrap: wrap;
 
     > li {
-      background: #d9d9d9;
+      $bg: #d9d9d9;
+      background: $bg;
       $h: 24px;
       height: $h;
       line-height: $h;
@@ -40,6 +56,10 @@ export default class Tags extends Vue{
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
+      &.selected {
+        background: darken($bg,50%);
+        color: white;
+      }
     }
   }
 
