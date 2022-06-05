b09d7575875fd5bf2d70cd869ead2586f67f18cf
niuniu315 <454378439@qq.com>
2022-06-05 18:08:52 +0800

    完成EditLabel组件id路由

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,14 +1,25 @@
 <template>
-
+  <div>编辑标签页面</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import tagListModel from '@/models/tagListModel';
 
 @Component
 export default class EditLabel extends Vue {
+  created() {
+    tagListModel.fetch();
+    const id = this.$route.params.id;
+    const tags = tagListModel.data;
+    const tag = tags.filter(t => t.id === id)[0]; //获取到的是个数组  数组里面的第0个
+    if (tag) {
 
+    } else {
+      this.$router.replace('/404');
+    }
+  }
 }
 </script>
 
