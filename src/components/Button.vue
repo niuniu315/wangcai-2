3b3760219869308a5422d2ecbf08f0d73d434e1a
niuniu315 <454378439@qq.com>
2022-06-07 10:43:02 +0800

    完成编辑标签css样式

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -16,4 +16,18 @@ export default class Button extends Vue {
 
 <style lang="scss" scoped>
 
+.button {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
 </style>
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,10 +1,17 @@
 <template>
   <Layout>
-    <div>
-      <Icon name="left"/>
-      <span>编辑标签</span>
+    <div class="navBar">
+      <Icon class="leftIcon" name="left"/>
+      <span class="title">编辑标签</span>
+      <span class="rightIcon"></span>
+      <!--      tip:多加一个标签好平均布局-->
     </div>
-    <FormItem field-name="标签名" placeholder="请输入标签名"/>
+    <div class="form-wrapper">
+      <FormItem field-name="标签名" placeholder="请输入标签名"/>
+    </div>
+    <div class="button-wrapper">
+      <Button>删除标签</Button>
+    </div>
   </Layout>
 </template>
 
@@ -13,9 +20,10 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
 import FormItem from '@/components/money/FormItem.vue';
+import Button from '@/components/Button.vue';
 
 @Component({
-  components: {FormItem}
+  components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
   created() {
@@ -33,5 +41,29 @@ export default class EditLabel extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.navBar {
+  text-align: center;
+  font-size: 16px;
+  padding: 12px 16px;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
+  > .leftIcon {
+    width: 24px;
+    height: 24px;
+  }
+}
+
+.form-wrapper {
+  background: white;
+  margin-top: 8px;
+}
+
+.button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
+}
 </style>
