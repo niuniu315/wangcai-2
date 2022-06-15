a33556e5a3d2ecaf0d816251d1d3acd341dcbdf7
niuniu315 <454378439@qq.com>
2022-06-15 23:27:55 +0800

    解决bug

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,4 +1,5 @@
 import createdId from '@/lib/createdId';
+
 const localStorageLeyName = 'tagList';
 type Tag = {
   id: string;
@@ -23,8 +24,8 @@ const tagListModel: TagListModel = {
     if (names.indexOf(name) >= 0) {
       return 'duplicated';
     }
-    const id = createdId().toString()
-    this.data.push({id: name, name: name});
+    const id = createdId().toString();
+    this.data.push({id, name: name});
     this.save();
     return 'success';
   },
