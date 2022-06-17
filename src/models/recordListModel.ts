b73a5b6a8b3cb3bfc312c0dc7f598d9a9fd34f42
niuniu315 <454378439@qq.com>
2022-06-17 22:16:22 +0800

    重新封装recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,13 +1,15 @@
 const localStorageKeyName = 'recordList';
 const recordListModel = {
+  data: [] as RecordType[],
   clone(data: RecordType | RecordType[]) {
     return JSON.parse(JSON.stringify(data));
   },
   fetch() {
-    return JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') as RecordType[]; // 强制类型是个RecordType[]
+    this.data = JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') as RecordType[]; // 强制类型是个RecordType[])
+    return this.data;
   },
-  save(data: RecordType[]) {
-    window.localStorage.setItem('localStorageKeyName', JSON.stringify(data));
+  save() {
+    window.localStorage.setItem('localStorageKeyName', JSON.stringify(this.data));
   }
 };
 
