e37c877a5a01068f25a09bf3404dd0bbde130eff
niuniu315 <454378439@qq.com>
2022-06-28 23:24:15 +0800

    使用classPrefix 设置高度

--- src/components/money/Tabs.vue
+++ src/components/money/Tabs.vue
@@ -1,8 +1,9 @@
 <template>
-  <ul class="tabs">
+  <ul class="tabs" :class="{[classPrefix+'-tabs']: classPrefix}">
     <li v-for="item in dataSource" :key="item.value"
         :class="liClass(item)"
-        @click="select(item)">
+        @click="select(item)"
+        class="tabs-item">
       {{ item.text }}
     </li>
   </ul>
@@ -42,13 +43,15 @@ export default class Tabs extends Vue {
   display: flex;
   text-align: center;
   font-size: 24px;
-  > li {
+
+  &-item {
     width: 50%;
     height: 64px;
     display: flex;
     justify-content: center;
     align-items: center;
     position: relative;
+
     &.selected::after {
       content: '';
       position: absolute;
