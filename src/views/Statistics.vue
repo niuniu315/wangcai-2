da992bba43d4cbe303670cd83fe0a6b96be1a022
niuniu315 <454378439@qq.com>
2022-07-03 22:41:15 +0800

    设置创建时间格式（今天昨天前天...）

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,7 +5,7 @@
     <div>
       <ol>
         <li v-for="(group,index) in result" :key="index">
-          <h3 class="title">{{ group.title }}</h3>
+          <h3 class="title">{{ beautify(group.title) }}</h3>
           <ol>
             <li v-for="item in group.items" :key="item.id"
                 class="record">
@@ -26,6 +26,7 @@ import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/money/Tabs.vue';
 import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
+import dayjs from 'dayjs';
 
 @Component({
   components: {Tabs},
@@ -33,6 +34,22 @@ import recordTypeList from '@/constants/recordTypeList';
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
     return tags.length === 0 ? '无' : tags.join(',');
+  }
+
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天';
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日');
+    } else {
+      return day.format(('YYYY年M月D日'));
+    }
   }
 
   get recordList() {
