83abb62c64e7b0631661baebf569e6907230f461
niuniu315 <454378439@qq.com>
2022-07-05 00:04:58 +0800

    重新设置页面宽度

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,11 @@ body {
   font-family: $font-hei;
   line-height: 1.5;
   background: #f5f5f5;
+  max-width: 500px;
+  margin: 0 auto;
 }
 
 #app {
-  max-width: 500px;
-  margin: 0 auto;
+
 }
 </style>
