587603737ddccabb96ed27ca74b44102db2b1acd
niuniu315 <454378439@qq.com>
2022-07-04 18:17:43 +0800

    完成1.统计列表的数据分组 2.当天+、-的总额，发现数字类型错误

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,10 +4,11 @@
     <div>
       <ol>
         <li v-for="(group,index) in groupedList" :key="index">
-          <h3 class="title">{{ beautify(group.title) }}</h3>
+          <h3 class="title">{{ beautify(group.title) }}
+            <span>￥{{ group.total }}</span>
+          </h3>
           <ol>
-            <li v-for="item in group.items" :key="item.id"
-                class="record">
+            <li v-for="item in group.items" :key="item.id" class="record">
               <span>{{ tagString(item.tags) }}</span>
               <span class="notes">备注:{{ item.notes }}</span>
               <span>￥{{ item.amount }} </span>
@@ -25,6 +26,7 @@ import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/money/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
+import clone from '@/lib/clone';
 
 @Component({
   components: {Tabs},
@@ -54,18 +56,42 @@ export default class Statistics extends Vue {
     return (this.$store.state as RootState).recordList;
   }
 
-
+//分组后的列表
+// 克隆 = 过滤掉未排序的列表 和 选择的类型（支出或收入）
+// .sort() 方法用原地算法对数组的元素进行排序，并返回数组。
+// 将后一天的时间 - 前一天的时间 进行排序
+// format(格式)
+// result(结果) 类型格式{
+//    title:时间 , items[]:过滤后的新数组的第一项  , total:合计金额
+// }
   get groupedList() {
     const {recordList} = this;
-    type HashTableValue = { title: string, items: RecordType[] }
-    const hashTable: { [key: string]: HashTableValue } = {};
+    if (recordList.length === 0) {return [];}  // 判断有无recordList数据，如果无，return[]
+    const newList = clone(recordList)
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createdTime).valueOf() - dayjs(a.createdTime).valueOf());
 
-    for (let i = 0; i < recordList.length; i++) {
-      const [date, time] = recordList[i].createdTime!.split('T');
-      hashTable[date] = hashTable[date] || {title: date, items: []};
-      hashTable[date].items.push(recordList[i]);
+    type Result = { title: string, total?: number, items: RecordType[] }[]
+
+    const result: Result = [{title: dayjs(newList[0].createdTime).format('YYYY-MM-DD'), items: [newList[0]]}];
+    for (let i = 1; i < newList.length; i++) {
+      // 判断：现在的列表 = 克隆后的列表的第i项
+      const current = newList[i];
+      const last = result[result.length - 1];
+      // 判断结果的最后一项的title和current的时间是否是同一天 ， 就将最后一项push到 i 项
+      // 反之结果的其他数据 push到新的 i 项里面
+      if (dayjs(last.title).isSame(dayjs(current.createdTime), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({title: dayjs(current.createdTime).format('YYYY-MM-DD'), items: [current]});
+      }
     }
-    return hashTable;
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => {
+        return sum + item.amount;
+      }, 0);
+    });
+    return result;
   }
 
   beforeCreate() {
