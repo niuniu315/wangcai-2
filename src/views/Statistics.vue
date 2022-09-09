fdb1dc680e0904040646eec98a38fd7494797c99
niuniu315 <454378439@qq.com>
2022-09-09 16:41:10 +0800

    完善图表样式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -78,13 +78,18 @@ export default class Statistics extends Vue {
           '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
           '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
           '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-        ]
+        ],
+        axisTick: {alignWithLabel: true},
+        axisLine: {lineStyle: {color: '#666'}}
       },
       yAxis: {
         type: 'value',
         show: false
       },
       series: [{
+        symbol: 'circle',
+        symbolSize: 12,
+        itemStyle: {borderWidth: 1, color: '#666', borderColor: '#666'},
         data: [
           820, 932, 901, 934, 1290, 1330, 1320,
           820, 932, 901, 934, 1290, 1330, 1320,
@@ -93,7 +98,11 @@ export default class Statistics extends Vue {
         ],
         type: 'line'
       }],
-      tooltip: {show: true}
+      tooltip: {
+        show: true, triggerOn: 'click',
+        position: 'top',
+        formatter: '{c}'
+      }
     };
   }
 
@@ -212,6 +221,7 @@ footer {
 
   &-wrapper {
     overflow: auto;
+
     &::-webkit-scrollbar {
       display: none;
     }
