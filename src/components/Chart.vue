faf0f54b91a9dd579b0781de953e2bb14c5eddb9
niuniu315 <454378439@qq.com>
2022-09-09 23:25:35 +0800

    自动更新options

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,28 +1,34 @@
 <template>
-<div class="wrapper" ref="wrapper">1121</div>
+  <div class="wrapper" ref="wrapper">1121</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
+import {Component, Prop, Watch} from 'vue-property-decorator';
 import * as echarts from 'echarts';
 import {EChartsOption} from 'echarts/types/dist/echarts';
 
 @Component
 export default class Chart extends Vue {
- @Prop() options?: EChartsOption;
+  @Prop() options?: EChartsOption;
+
   mounted() {
-    if (this.options === undefined){
-      return console.error('options 为空')
+    if (this.options === undefined) {
+      return console.error('options 为空');
     }
-    const chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
-    chart.setOption(this.options)
+    this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
+    this.chart.setOption(this.options);
   }
+
+@Watch('options')
+  onOptionsChange(newValue: EChartsOption) {
+    this.chart!.setOption(newValue)
+}
 }
 </script>
 
 <style lang="scss" scoped>
-.wrapper{
+.wrapper {
   height: 400px;
 }
 </style>
